a454a57d5168299d66d5fc46940d43e677732a4f
Kris Sakarias <ksakarias@gmail.com>
2021-04-26 17:50:31 -0700

    db migration testing

--- trails/models.py
+++ trails/models.py
@@ -2,6 +2,9 @@ import uuid
 from django.db import models
 
 class Trail(models.Model):
+  def __str__(self):
+    return self.name
+
   REGION_CHOICES = [
     ('OP', 'Olympic Peninsula'),
     ('NC', 'North Cascades'),
@@ -10,7 +13,7 @@ class Trail(models.Model):
     ('SC', 'South Cascades'),
     ('EW', 'Eastern Washington'),
     ('CW', 'Central Washington'),
-    ('WWL', 'Western Washington Lowlands'),
+    ('WW', 'Western Washington Lowlands'),
     ('SW', 'Southwest Washington'),
   ]
 
@@ -24,15 +27,18 @@ class Trail(models.Model):
     help_text='Geographic region in Washington where trail is located'
   )
   coordinates = models.CharField(
-    max_length=25
+    max_length=25,
     help_text='Geographic coordinates searchable via Google Maps'
   )
   length = models.DecimalField(
-    max_digits=4, decimal_places=1, help_text='From 0.0 to 999.9 miles', blank=True
+    max_digits=4, decimal_places=1, help_text='From 0.0 to 999.9 miles', blank=True, null=True
   )
-  elevation_gain = models.IntegerField('Elevation Gain', blank=True)
+  elevation_gain = models.IntegerField('Elevation Gain', blank=True, null=True)
 
 class Trailhead(models.Model):
+  def __str__(self):
+    return self.name + ' - ' + self.trail.name
+
   PARKING_TYPES = [
     ('UL', 'Unpaved Lot'),
     ('PL', 'Paved Lot'),
@@ -52,26 +58,28 @@ class Trailhead(models.Model):
 
   name = models.CharField(max_length=50, unique=True)
   coordinates = models.CharField(
-    max_length=25
+    max_length=25,
     help_text='Geographic coordinates searchable via Google Maps'
   )
 
   pkg_type = models.CharField(
-    max_length=2,
+    max_length=2, blank=True, null=True,
     choices=PARKING_TYPES,
     help_text='Type of Parking at Trailhead'
-    blank=True
   )
   pkg_capacity = models.IntegerField(
-    'Parking Capacity', blank=True,
+    'Parking Capacity', blank=True, null=True,
     help_text='Approximate number of cars capable of parking at trailhead lot',
   )
   bathroom = models.CharField(
-    help_text='Is there a bathroom at the trailhead?', blank=True
+    help_text='Is there a bathroom at the trailhead?', blank=True, null=True,
     max_length=1, choices=BATHROOM_STATUS
   )
 
 class Report(models.Model):
+  def __str__(self):
+    return str(self.modified)
+
   PARKING_TYPES = [
     ('UL', 'Unpaved Lot'),
     ('PL', 'Paved Lot'),
@@ -111,7 +119,7 @@ class Report(models.Model):
   trail_begin = models.TimeField(help_text='What time did the hike begin?')
   trail_end = models.TimeField(help_text='What time did the hike end?')
   bathroom = models.CharField(
-    help_text='Is there a bathroom at the trailhead?', blank=True
+    help_text='Is there a bathroom at the trailhead?', blank=True, null=True,
     max_length=1, choices=BATHROOM_STATUS
   )
   pkg_location = models.CharField(
