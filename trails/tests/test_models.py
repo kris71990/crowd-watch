24fa9f4603ab86755ff306844c8ebbc3ad87b86f
Kris Sakarias <ksakarias@gmail.com>
2021-05-05 12:21:30 -0700

    wrote tests for models, outlined tests for views

--- trails/tests/test_models.py
+++ trails/tests/test_models.py
@@ -1,11 +1,47 @@
 from django.test import TestCase
+from datetime import datetime
 
-from ..models import Trail
+from ..models import Trail, Trailhead, Report
+
+def create_trail(name, region, coordinates):
+  return Trail.objects.create(name=name, region=region, coordinates=coordinates)
+
+def create_trailhead(trail, name, coordinates):
+  return Trailhead.objects.create(trail=trail, name=name, coordinates=coordinates)
+
+def create_report(report):
+  return Report.objects.create(
+    trail=report['trail'], trailhead=report['trailhead'], date_hiked=report['date_hiked'], day_hiked=report['day_hiked'], trail_begin=report['trail_begin'], trail_end=report['trail_end'], bathroom=report['bathroom'], pkg_location=report['pkg_location'], pkg_estimate_begin=report['pkg_estimate_begin'], pkg_estimate_end=report['pkg_estimate_end'], cars_seen=report['cars_seen'], people_seen=report['people_seen'], horses_seen=report['horses_seen'], dogs_seen=report['dogs_seen']
+  )
+
 
 class TrailModelTests(TestCase):
   # create trail with all required fields returns a trail
   def test_create_trail(self):
-    trail = Trail(name='test_name', region='CC', coordinates='-23.44, -11.44')
+    trail = create_trail(name='test_name', region='CC', coordinates='-23.44, -11.44')
     self.assertIs(trail.name, 'test_name')
     self.assertIs(trail.region, 'CC')
     self.assertIs(trail.coordinates, '-23.44, -11.44')
+
+class TrailheadModelTests(TestCase):
+  # create trailhead for a trail, returns trailhead
+  def test_create_trailhead(self):
+    trail = create_trail(name='test_name', region='CC', coordinates='4352')
+    trailhead = create_trailhead(trail=trail, name='th', coordinates='4535')
+    self.assertIs(trailhead.name, 'th')
+    self.assertIs(trailhead.trail.name, 'test_name')
+
+class ReportModelTests(TestCase):
+  # create report for a trailhead/trail
+  def test_create_report(self):
+    trail = create_trail(name='test_name', region='CC', coordinates='4352')
+    trailhead = create_trailhead(trail=trail, name='th', coordinates='4535')
+    time = datetime.now()
+
+    report = create_report(report={ 
+      'trail': trail, 'trailhead': trailhead, 'date_hiked': time.date(), 'day_hiked': 'Th', 'trail_begin': time.time(), 'trail_end': time.time(), 'bathroom': 'N', 'pkg_location': 'P', 'pkg_estimate_begin': 15, 'pkg_estimate_end': 50, 'cars_seen': 30, 'people_seen': 100, 'horses_seen': False, 'dogs_seen': True
+    })
+
+    self.assertIs(report.cars_seen, 30)
+    self.assertIs(report.trail.name, 'test_name')
+    self.assertIs(report.trailhead.name, 'th')
--- trails/tests/test_views.py
+++ trails/tests/test_views.py
@@ -1,11 +1,36 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from ..models import Trail
+from ..models import Trail, Trailhead, Report
 
 class RegionViewTests(TestCase):
+  # /regions - returns a list of regions
   def test_region_list(self):
     response = self.client.get(reverse('regions'))
     self.assertEqual(response.status_code, 200)
     self.assertEqual(len(response.context['regions_list']), 9)
     self.assertIsInstance(response.context['regions_list'][0], tuple)
+
+class TrailViewTests(TestCase):
+  # /list
+  # returns empty list when no trails exist
+  def test_trail_list_empty(self):
+    response = self.client.get(reverse('trail_list'))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, 'No trails found')
+    self.assertQuerysetEqual(response.context['trails_list'], [])
+
+  # /trail_list - all trails when some exist
+  # /trail_list - all trails when some exist from multiple regions
+  # /trail_list - all trails ordered correctly
+
+  # /<region> - returns all trails in region
+  # /<region> - create a trail, return new list of trails
+
+
+# class TrailheadViewTests(TestCase)
+  # /<region>/<trail> - returns all trailheads for trail 
+
+# class ReportViewTests(TestCase):
+  # /<region>/<trail>/<reports> - returns all reports for trail
+  # /<region>/<trail>/<trailhead> - returns all reports for trailhead
