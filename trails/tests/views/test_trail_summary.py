8bd1cfbc1cfa67a0002d7cb668fc83aa09fe67dd
kris71990 <ksakarias@gmail.com>
2022-05-21 12:28:29 -0700

    fix trail summary test

--- trails/tests/views/test_trail_summary.py
+++ trails/tests/views/test_trail_summary.py
@@ -3,22 +3,25 @@ from django.urls import reverse
 from faker import Faker
 
 from ...models import Trail
-from ..mocks import create_trail, create_trail_and_trailhead
+from ..mocks import create_region, create_trail_and_trailhead
 
 fake = Faker()
 
 # /<region>/<trail>/summary
 class TrailSummaryViewTests(TestCase):
   # returns summary of trail data
-  def test_trail_summary(self):
-    region = 'CC'
-    trailheads = create_trail_and_trailhead(name=fake.name(), region=region, coordinates=fake.word(), filters=None)
+  def test_trail_summary_no_reports(self):
+    region = create_region('CC')
+    trailhead = create_trail_and_trailhead(region=region, name=fake.name(), filters=None)
+    trailhead_trails = trailhead.trails.all()
 
-    response = self.client.get(reverse('trail_summary', args=(region, trailheads.trail.id,)))
+    response = self.client.get(reverse('trail_summary', args=(region.region_slug, trailhead_trails[0].trail_slug,)))
 
     self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed('trail_summary.html')
     self.assertContains(response, 'Summary')
-    self.assertEqual(response.context['trail'], trailheads.trail)
+    self.assertEqual(response.context['trail'], trailhead_trails[0])
     self.assertEqual(response.context['region'], region)
-    self.assertQuerysetEqual(response.context['trailheads'][0].name, trailheads.name)
+    self.assertQuerysetEqual(response.context['trailheads']['obj'][0].name, trailhead.name)
+    self.assertEqual(response.context['summary']['reports_region_count'], 0)
+    self.assertEqual(response.context['summary']['reports_region_count'], 0)
