4ba0e6ca8611da1171764c35898ae99bea25704b
kris71990 <ksakarias@gmail.com>
2022-05-20 17:56:27 -0700

    fix trail view tests

--- trails/tests/views/test_region_views.py
+++ trails/tests/views/test_region_views.py
@@ -1,11 +1,18 @@
 from django.test import TestCase
 
+from ...models import Region
+from ..mocks import create_region
+
 # /regions
 class RegionViewTests(TestCase):
   # returns a list of regions
   def test_region_list(self):
+    region = create_region('CC')
     response = self.client.get('/trails/regions/')
+
     self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed('regions.html')
-    self.assertEqual(len(response.context['regions_list']), 9)
-    self.assertIsInstance(response.context['regions_list'][0], tuple)
+    self.assertEqual(response.context['regions_table'].all()[0].name, region.name)
+    self.assertEqual(response.context['regions_table'].all()[0].reports, 0)
+    self.assertEqual(response.context['regions_table'].all()[0].trails, 0)
+    self.assertEqual(response.context['regions_table'].all()[0].trailheads, 0)
